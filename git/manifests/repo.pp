23d7003008c2cfc1ab2b7db885e23562366e0d72
Ellie Frost <web@stillinbeta.com>
2013-02-23 14:07:53 -0800

    Allow custom paths with git

--- git/manifests/repo.pp
+++ git/manifests/repo.pp
@@ -1,13 +1,22 @@
-define git::repo ($name = $title, $branch = 'master', $service = false) {
+define git::repo (
+    $name = $title,
+    $branch = 'master',
+    $service = false
+    $bare_path = false
+    $working_path = false
+    ) {
     require git
-    require stillinbeta-user  
+    require stillinbeta-user
 
-    realize File["/home/stillinbeta/git"]
-    realize File["/home/stillinbeta/sites"]
+    unless $bare_path {
+        realize File["/home/stillinbeta/git"]
+        $bare_path = "/home/stillinbeta/git/${name}.git"
+    }
+    unless $working_path {
+        realize File["/home/stillinbeta/sites"]
+        $working_path = "/home/stillinbeta/sites/${name}"
+    }
 
-    $bare_path = "/home/stillinbeta/git/${name}.git" 
-    $working_path = "/home/stillinbeta/sites/${name}" 
-    
     # Git init is safe to rerun
     exec { "git_init_${name}":
         command => "${git::git_bin} init --bare $bare_path",
