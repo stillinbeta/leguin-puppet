5650e7c571f78337d94234ee24a1c6710b81cb12
Ellie Frost <web@stillinbeta.com>
2013-03-19 00:23:16 +1100

    Tweaks to gunicorn

--- gunicorn/manifests/django.pp
+++ gunicorn/manifests/django.pp
@@ -1,20 +1,35 @@
 define gunicorn::django (
     $site_name = $title,
+    $domain,
     $directory,
+    $static_dir = false,
 ) {
     include nginx
 
-    python::pip { "$directory-gunicorn":
+    python::pip { "$directory-gunicorn": 
         virtualenv => $directory
     }
 
-    file { "$directory/django.wsgi":
-        ensure => exists,
-        contents => template("gunicorn/django.wsgi.erb"),
+    file { "$directory/django-wsgi.py":
+        ensure => file,
+        content => template("gunicorn/django.wsgi.erb"),
     }
 
     file { "/etc/init/$site_name-gunicorn.conf":
-        ensure => exists,
-        contents => template("gunicorn/django_upstart.conf.erb"),
+        ensure => file,
+        content => template("gunicorn/django_upstart.conf.erb"),
+    }
+
+    file { "/etc/nginx/sites-available/$title.conf":
+        ensure => file,
+        content => template("gunicorn/nginx-template.erb"),
+        require => Package['nginx'],
+    }
+
+    file { "/etc/nginx/sites-enabled/$title.conf":
+        ensure => link,
+        target => "/etc/nginx/sites-available/$title.conf",
+        notify => Service['nginx'],
+        require => Package['nginx'],
     }
 }
