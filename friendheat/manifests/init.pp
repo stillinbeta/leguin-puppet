ebf74faf68ad2a61012ab4c412473ebb513a0814
Ellie Frost <web@stillinbeta.com>
2013-03-24 17:39:40 -0700

    First iteration of friendheat configuration

---
+++ friendheat/manifests/init.pp
@@ -0,0 +1,25 @@
+class friendheat {
+    require stillinbeta-user
+
+    $site_path = '/home/stillinbeta/sites/friendheat'
+
+    git::repo {'friendheat':
+        service => 'friendheat-tornado',
+        working_path => "$site_path",
+    }
+
+    python::virtualenv { $site_path:
+        packages => ['tornado', 'sqlite3dbm']
+    }
+
+    file { '/etc/init/friendheat-tornado.conf':
+        ensure => file,
+        source => 'puppet:///modules/friendheat/friendheat-tornado.conf',
+    }
+
+    nginx::proxypass { "friendheat":
+        server_domain => 'friendheat.stillinbeta.com',
+        socket => '/tmp/friendheat-tornado.socket',
+        static_dir => "$site_path/static",
+    }
+}
